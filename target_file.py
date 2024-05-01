ca6c3e8edc06ceb2cf1e9c9973b52f82aca10a0b
dong881 <joe888joejoe@gmail.com>
2024-05-01 16:38:50 +0200

    save current status

--- target_file.py
+++ target_file.py
@@ -1,68 +1,59 @@
 
 import random
 import time
-
 class SnakeGame:
     def __init__(self):
-        self.grid_size = (20, 20)
-        self.snake = [(10, 10), (9, 10), (8, 10)]
-        self.food = None
-        self.direction = 'down'
-        self.score = 0
-
+        self.grid_size=(20,20)
+        self.snake=[(10,10),(9,10),(8,10)]
+        self.food=None
+        self.direction='down'
+        self.score=0
     def generate_food(self):
         while True:
-            x = random.randint(0, self.grid_size[0] - 1)
-            y = random.randint(0, self.grid_size[1] - 1)
-            if (x, y) not in self.snake and (x, y) != (10, 10):
-                self.food = (x, y)
+            x=random.randint(0,self.grid_size[0]-1)
+            y=random.randint(0,self.grid_size[1]-1)
+            if (x,y) not in self.snake and (x,y)!=(10,10):
+                self.food=(x,y)
                 break
-
     def move_snake(self):
-        head = self.snake[0]
-        if self.direction == 'up':
-            new_head = (head[0], head[1] - 1)
-        elif self.direction == 'down':
-            new_head = (head[0], head[1] + 1)
-        elif self.direction == 'left':
-            new_head = (head[0] - 1, head[1])
+        head=self.snake[0]
+        if self.direction=='up':
+            new_head=(head[0],head[1]-1)
+        elif self.direction=='down':
+            new_head=(head[0],head[1]+1)
+        elif self.direction=='left':
+            new_head=(head[0]-1,head[1])
         else:
-            new_head = (head[0] + 1, head[1])
-
-        if new_head in self.snake or new_head[0] < 0 or new_head[0] >= self.grid_size[0] or new_head[1] < 0 or new_head[1] >= self.grid_size[1]:
+            new_head=(head[0]+1,head[1])
+        if new_head in self.snake or new_head[0]<0 or new_head[0]>=self.grid_size[0] or new_head[1]<0 or new_head[1]>=self.grid_size[1]:
             return False
+        self.snake.insert(0,new_head)
+        if self.food and self.food==new_head:
+            del self.food
         else:
-            self.snake.insert(0, new_head)
-            if self.food and self.food == new_head:
-                self.score += 1
-                self.generate_food()
-            else:
-                self.snake.pop()
-
+            self.snake.pop()
+        return True
     def play_game(self):
         while True:
-            print("Score: " + str(self.score))
-            for y in range(self.grid_size[1]):
-                for x in range(self.grid_size[0]):
-                    if (x, y) == self.food:
-                        print('F', end=' ')
-                    elif (x, y) in self.snake:
-                        print('S', end=' ')
+            for x in range(self.grid_size[0]):
+                for y in range(self.grid_size[1]):
+                    if (x,y) in self.snake:
+                        print('*',end=' ')
+                    elif (x,y)==self.food:
+                        print('F',end=' ')
                     else:
-                        print(' ', end=' ')
+                        print('~',end=' ')
                 print()
-            command = input("Enter a direction (up/down/left/right) or 'quit' to exit: ").lower()
-            if command == 'quit':
+            command=input("Enter a direction (up/down/left/right) or 'quit' to exit: ").lower()
+            if command=='quit':
                 break
-            elif command in ['up', 'down', 'left', 'right']:
-                self.direction = command
+            elif command in ['up','down','left','right']:
+                self.direction=command
             else:
                 continue
-
             while not self.move_snake():
-                print("Game Over! Final score: " + str(self.score))
+                print("Game Over! Final score: ",self.score)
                 return
-
-game = SnakeGame()
+game=SnakeGame()
 while True:
     game.play_game()
