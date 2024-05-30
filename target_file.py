2115e91146957f7a5fb90edefaa2a067a5c3bc9f
dong881 <joe888joejoe@gmail.com>
2024-05-30 20:44:30 +0200

    Here is the output of the `git diff` command in a simple and clear bulleted list format:
    **Changes between two commits**
    
    * **Removed lines**
    	+ Removed `screen_width`, `screen_height`, `snake_speed` and other variables
    	+ Removed `Snake` class and its methods
    	+ Removed `generate_apple()` function
    * **Added lines**
    	+ Added `snake = [(200, 100), (220, 100), (240, 100)]` and `direction = 'right'`
    	+ Added `food = None` and `score = 0`
    	+ Added game loop with event handling and drawing code
    * **Modified lines**
    	+ Modified `size` variable to `(800, 600)`
    	+ Modified `snake` and `food` variables in the game loop
    
    Let me know if you have any questions or need further clarification!

--- target_file.py
+++ target_file.py
@@ -5,79 +5,63 @@ import sys
 
 pygame.init()
 
-screen_width = 800
-screen_height = 600
-snake_speed = 10
-
-size = width, height = 640, 480
+size = width, height = 800, 600
 black = 0, 0, 0
 white = 255, 255, 255
-snake_color = 100, 50, 30
-apple_color = 200, 100, 50
 
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Snake Game")
 
-class Snake:
-    def __init__(self):
-        self.length = 1
-        self.body = [[width/2, height/2]]
-        self.direction = 'right'
+snake = [(200, 100), (220, 100), (240, 100)]
+direction = 'right'
 
-    def move(self):
-        if self.direction == 'right':
-            new_head = [self.body[-1][0] + 10, self.body[-1][1]]
-        elif self.direction == 'left':
-            new_head = [self.body[-1][0] - 10, self.body[-1][1]]
-        elif self.direction == 'up':
-            new_head = [self.body[-1][0], self.body[-1][1] - 10]
-        elif self.direction == 'down':
-            new_head = [self.body[-1][0], self.body[-1][1] + 10]
+food = None
+score = 0
 
-        if new_head in self.body[:-1]:
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and direction != 'down':
+                direction = 'up'
+            elif event.key == pygame.K_DOWN and direction != 'up':
+                direction = 'down'
+            elif event.key == pygame.K_LEFT and direction != 'right':
+                direction = 'left'
+            elif event.key == pygame.K_RIGHT and direction != 'left':
+                direction = 'right'
 
-        self.body.append(new_head)
+    if food is None:
+        food = (random.randint(0, width-10), random.randint(0, height-10))
 
-    def eat_apple(self, apple):
-        if self.body[-1] == apple:
-            return True
-        else:
-            return False
+    head = snake[0]
+    if direction == 'up':
+        new_head = (head[0], head[1] - 10)
+    elif direction == 'down':
+        new_head = (head[0], head[1] + 10)
+    elif direction == 'left':
+        new_head = (head[0] - 10, head[1])
+    elif direction == 'right':
+        new_head = (head[0] + 10, head[1])
 
-def generate_apple():
-    return [random.randint(0, width-10), random.randint(0, height-10)]
+    snake.insert(0, new_head)
 
-def main_game_loop(snake, apple):
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT and snake.direction != 'right':
-                    snake.direction = 'left'
-                elif event.key == pygame.K_RIGHT and snake.direction != 'left':
-                    snake.direction = 'right'
-                elif event.key == pygame.K_UP and snake.direction != 'down':
-                    snake.direction = 'up'
-                elif event.key == pygame.K_DOWN and snake.direction != 'up':
-                    snake.direction = 'down'
+    if head == food:
+        score += 1
+        while True:
+            food = (random.randint(0, width-10), random.randint(0, height-10))
+            for part in snake:
+                if food == part:
+                    break
+            else:
+                break
+    else:
+        snake.pop()
 
-        snake.move()
+    screen.fill(black)
+    for point in snake:
+        pygame.draw.rect(screen, white, pygame.Rect(point[0], point[1], 10, 10))
+    pygame.draw.rect(screen, (255,165,0), pygame.Rect(food[0], food[1], 10, 10))
 
-        if not snake.eat_apple(apple):
-            apple = generate_apple()
-
-        screen.fill(black)
-        for part in snake.body:
-            pygame.draw.rect(screen, snake_color, pygame.Rect(part[0], part[1], 10, 10))
-        pygame.draw.rect(screen, apple_color, pygame.Rect(apple[0], apple[1], 10, 10))
-
-        pygame.display.flip()
-
-snake = Snake()
-apple = generate_apple()
-
-main_game_loop(snake, apple)
+    pygame.display.flip()
